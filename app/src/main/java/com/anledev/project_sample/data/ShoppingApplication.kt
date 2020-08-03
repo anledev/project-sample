cf91d044b54b7ae06d091a0487a64dc07044850f
frank <anlt.zx@gmail.com>
2020-08-03 11:33:43 +0700

    after add kodein

---
+++ app/src/main/java/com/anledev/project_sample/data/ShoppingApplication.kt
@@ -0,0 +1,37 @@
+package com.anledev.project_sample.data
+
+import android.app.Application
+import com.anledev.project_sample.data.db.ShoppingDatabase
+import com.anledev.project_sample.data.repository.ShoppingRepository
+import com.anledev.project_sample.data.ui.shoppinglist.ShoppingViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class ShoppingApplication: Application(), KodeinAware {
+//    override val kodein: Kodein = Kodein.lazy {
+//        import(androidXModule(this@ShoppingApplication))
+//        bind() from singleton { ShoppingDatabase(instance()) }
+//        bind() from singleton { ShoppingRepository(instance()) }
+//        bind() from provider { ShoppingViewModelFactory(instance()) }
+//    }
+
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ShoppingApplication))
+        bind() from singleton { ShoppingDatabase(instance()) }
+        bind() from singleton {
+            ShoppingRepository(
+                instance()
+            )
+        }
+        bind() from provider {
+            ShoppingViewModelFactory(
+                instance()
+            )
+        }
+    }
+}
--- app/src/main/java/com/anledev/project_sample/data/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/anledev/project_sample/data/ui/shoppinglist/ShoppingActivity.kt
@@ -6,22 +6,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anledev.project_sample.R
-import com.anledev.project_sample.data.db.ShoppingDatabase
 import com.anledev.project_sample.data.others.ShoppingItemAdapter
-import com.anledev.project_sample.data.repository.ShoppingRepository
 import kotlinx.android.synthetic.main.activity_shopping.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
-
         val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
-
 
         val adapter = ShoppingItemAdapter(listOf(), viewModel)
 
